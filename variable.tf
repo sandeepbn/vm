ac61d15c5b4e245cbb6a980a111b595d68399883
Sandeep Nagaraju <Sandeep.Nagaraju@harman.com>
2019-06-14 12:52:20 +0530

    first commit

---
+++ variable.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+    subscription_id = "2363f169-607d-499e-aeea-28ebcdefd59a"
+    client_id       = "a658d025-1c38-40ed-bb3d-85575eb10d21"
+    client_secret   = "9adf3f4b-a2d5-417a-83dc-369d4210ca2b"
+    tenant_id       = "f66b6bd3-ebc2-4f54-8769-d22858de97c5"  
+}
---
+++ vmresource.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "test" {
+  name     = "production"
+  location = "West US"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "test" {
+  name                = "production-network"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  location            = "${azurerm_resource_group.test.location}"
+  address_space       = ["10.0.0.0/16"]
+}
